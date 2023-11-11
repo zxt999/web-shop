a22d609cce40aefe7d9b95c6a2fb1134de40974f
YmYuekun <147126698+YmYuekun@users.noreply.github.com>
2023-11-11 15:41:18 +1300

    BG changes from red to blue (#1)
    * BG changes from red to blue
    
    * remove g
    
    ---------
    
    Co-authored-by: Mona Lisa <yuekunyao@YuekundeAir.hub>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
 
   return (
-      <div style={{position:"absolute",width:"100%",height:"100%",background:'red'}}>
+      <div style={{position:"absolute",width:"100%",height:"100%",background:'blue'}}>
           <input style={{marginBottom:"15px",display:"flex", alignItems:'center',margin:"auto" }}/>
           <p style={{width:'100%', textAlign:'center'}}>name</p>
       </div>
